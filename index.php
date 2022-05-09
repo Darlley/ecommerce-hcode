986d1cd5a6863a1740d00b3c371d22746588e07c
Darlley <systembrtt@gmail.com>
2022-05-09 08:44:17 -0400

    autoload hcode classes

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ require_once("vendor/autoload.php");
 $app = new \Slim\Slim();
 $app->config('debug', true);
 $app->get('/', function() {
-	echo "OK";
+	$sql = new Hcode\DB\Sql();
+	$results = $sql->select("SELECT * FROM tb_users");
+	
+	echo json_encode($results);
 });
 $app->run();
