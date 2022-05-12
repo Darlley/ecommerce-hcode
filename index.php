745b0d8a1e4ed386116a56775675dba8650953bc
Darlley <systembrtt@gmail.com>
2022-05-12 09:31:44 -0400

    aula 08 errors

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 $app->config('debug', true);
@@ -20,9 +21,23 @@ $app->get('/', function() {
 /** 
  * ROTAS ADMIN
 */
+$app->get('/login', function() {
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl('login');
+});
 $app->get('/admin', function() {
 	$page = new PageAdmin();
 	$page->setTpl('index');
+});
+
+$app->post('/login', function(){
+	User::login($_POST["deslogin"], $_POST['despassword']);
+
+	header("Location: /admin");
+	exit;
 });
 
 $app->run();
--- views-cache/header.0d15a13d2980f1f3916c58a24ea8ae9d.rtpl.php
+++ views-cache/header.0d15a13d2980f1f3916c58a24ea8ae9d.rtpl.php
@@ -42,7 +42,7 @@
                             <li><a href="#"><i class="fa fa-user"></i> Minha Conta</a></li>
                             <li><a href="#"><i class="fa fa-heart"></i> Lista de Desejos</a></li>
                             <li><a href="#"><i class="fa fa-shopping-cart"></i> Meu Carrinho</a></li>
-                            <li><a href="#"><i class="fa fa-lock"></i> Login</a></li>
+                            <li><a href="/login"><i class="fa fa-lock"></i> Login</a></li>
                         </ul>
                     </div>
                 </div>
